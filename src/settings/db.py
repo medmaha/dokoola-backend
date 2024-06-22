87c1072c31c0614dc9b9fc1f195f81117642e1fa
medmaha <toure925@outlook.com>
2024-06-22 11:50:10 +0000

    Added default sqlite3 database

--- src/settings/db.py
+++ src/settings/db.py
@@ -4,7 +4,7 @@ DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
 default_db = {
     "ENGINE": "django.db.backends.sqlite3",
-    "NAME": os.path.join("_db", "db.sqlite"),
+    "NAME": "db.sqlite3",
 }
 
 if not DEBUG:
