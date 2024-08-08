5d6d323b63579c86606ef698864e9a1bb4547c78
Touray Mahammed <toure925@outlook.com>
2024-08-08 07:32:39 +0000

    Update csrf.py

--- core/middleware/csrf.py
+++ core/middleware/csrf.py
@@ -15,8 +15,30 @@ class DokoolaCSRFMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+
+    def check_if_samesite(self, request: HttpRequest):
+        # Check if the request is from the same domain
+        print(request.META.get("REMOTE_HOST"))
+
+        host = request.META.get("HTTP_HOST", "00")
+        
+        base_url = request.META.get("BASE_URL", "")
+        if host in base_url:
+            return True
+
+        allowed_host = request.META.get("ALLOWED_HOSTS")
+        if host in allowed_host:
+            return True
+        else:
+            return False
+
  
     def __call__(self, request: HttpRequest):
+        samesite = self.check_if_samesite(request)
+
+        if samesite:
+            print("Same site: Request from same domain")
+            return self.get_response(request)
 
         csrf_header = request.headers.get(os.environ.get("SERVICE_HTTP_HEADER", "_"))
 
