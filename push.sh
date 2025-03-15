4981548b10cfb92c6d21ccf49750ccc4a7837b00
medmaha <toure295@outlook.com>
2025-03-15 02:04:20 +0000

    chore: simplify push script by removing Docker login and tagging comments

--- push.sh
+++ push.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-# Ensure you're logged into Docker Hub
-# docker login
-
-# Tag the local image for Docker Hub
 docker tag dokoola-backend intrasoft0/dokoola:backend-001
-
-# Push to Docker Hub
 docker push intrasoft0/dokoola:backend-001
