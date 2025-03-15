e614be8dc529d55c178c12e4e2000576045bc40f
medmaha <toure295@outlook.com>
2025-03-15 03:05:32 +0000

    feat: pass GitHub SHA to push script for better image tagging

--- push.sh
+++ push.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-docker tag dokoola-backend intrasoft0/dokoola:backend-001
-docker push intrasoft0/dokoola:backend-001
+remote_image_tag="intrasoft0/dokoola:$1"
+
+# docker tag dokoola-backend $remote_image_tag
+# docker push intrasoft0/dokoola:backend-001
+
+# # remove the tagged image
+# docker rmi $remote_image_tag
